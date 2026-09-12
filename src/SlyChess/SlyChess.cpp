0cde4f02080dd189c60ac322672e37303a102a98
JavaCoder5 <111396266+JavaCoder5@users.noreply.github.com>
2026-09-12 15:43:57 -0400

    Bump version to 0.2

--- src/SlyChess/SlyChess.cpp
+++ src/SlyChess/SlyChess.cpp
@@ -259,7 +259,7 @@ int main() {
         if (line == "uci") {
             std::cout << "id name SlyChess\n";
             std::cout << "id author JavaCoder5\n";
-            std::cout << "id version 0.1\n";
+            std::cout << "id version 0.2\n";
             std::cout << "uciok\n" << std::flush;
         }
         else if (line == "isready") {
